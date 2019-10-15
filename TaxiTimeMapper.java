5682bc432443afdcdbd34af5e338e18a3cb2f5c6
sahajmarwah <44073831+sahajmarwah@users.noreply.github.com>
2019-10-15 18:23:05 -0400

    Add files via upload

---
+++ TaxiTimeMapper.java
@@ -0,0 +1,29 @@
+/**
+ * @description: The 3 airports with the longest and shortest average taxi time per flight (both in and out), respectively. Mapper
+ */
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TaxiTimeMapper extends Mapper<Object, Text, Text, Text> {
+	/**
+	 * Taxi Time
+	 */
+	@Override
+	protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		String[] infos = value.toString().split(",");
+		// exclude the first line
+		if (!"Year".equals(infos[0])) {
+			if (!"NA".equals(infos[20])) {
+				context.write(new Text(infos[16]), new Text(infos[20]));
+			}
+			if (!"NA".equals(infos[19])) {
+				context.write(new Text(infos[17]), new Text(infos[19]));
+			}
+
+		}
+	}
+}
